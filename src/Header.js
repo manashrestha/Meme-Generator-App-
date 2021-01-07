11e2ddf981fc7373f679fae98946c3cf33409650
Manash Shrestha <64713029+manashrestha@users.noreply.github.com>
2021-01-07 15:08:35 -0500

    First commit

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Header(){
+    return(
+        <header>
+            <img 
+            src="https://assets.stickpng.com/images/580b585b2edbce24c47b2a2c.png" 
+            alt="problem?" />
+            <p>Meme Generator</p>
+
+        </header>
+    )
+}
+
+export default Header
